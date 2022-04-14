2b5efbcdb9072256339684b2580536b990b84b26
Tetsuya4430 <k020g1184@g.neec.ac.jp>
2022-04-15 00:11:54 +0900

    01_05のライブラリ以外のコミット

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 //クラス化したヘッダファイルのインクルード
 #include "MyGame.h"
 
+#include "fbxsdk.h"
+
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	HRESULT result;
+
+	FbxManager* fbxManager = FbxManager::Create();
 
 	FrameWork* game = new MyGame();
 
